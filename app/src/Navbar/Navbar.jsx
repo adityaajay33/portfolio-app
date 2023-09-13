234ccf4cf8749b0cfa2c576d9163245f50a35d60
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-09-13 15:49:39 -0400

    completed navbar

--- app/src/Navbar/Navbar.jsx
+++ app/src/Navbar/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./Navbar.css"
+import Link from "next/link"
 
 export default function Navbar() {
   return (
     <div className="navbarDiv">
         <div className="logoDiv">
-            Aditya
+            <div>Aditya</div>
         </div>
         <div className="websitePages">
-            About
+            <ul className="navLinks">
+
+                <li className="indLink">
+                <Link href="/about" className="linkWeb">About</Link>
+                </li>
+                <li className="indLink">
+                <Link href="/projects" className="linkWeb">Projects</Link>
+                </li>
+                <li className="indLink">
+                <Link href="/experiences" className="linkWeb">Experiences</Link>
+                </li>
+            </ul>
         </div>
     </div>
   )
