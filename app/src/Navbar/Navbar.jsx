b34d17570143427be0190cea417bf2490506e9b1
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-09-13 14:04:06 -0400

    created navbar

---
+++ app/src/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className="navbarDiv">
+        <div className="logoDiv">
+            Aditya
+        </div>
+        <div className="websitePages">
+            About
+        </div>
+    </div>
+  )
+}
