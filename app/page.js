974b701ac0d1f167d453ee7d4923849993bacb1b
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-09-23 23:43:25 -0400

    modifications to front page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from './src/Navbar/Navbar'
+import Introduction from './src/Introduction/Introduction'
+
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Introduction/>
       <div className={styles.description}>
         <p className="titleName">
           Aditya Ajay
---
+++ app/src/Introduction/Introduction.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Introduction.css"
+
+const Introduction = () => {
+  return (
+    <div className="introductionBox">
+        <div className="profilePicture">
+            Hello
+        </div>
+        <div className="nameBackground">
+          <div className="nameBox">
+            Hi, I'm Aditya Ajay.
+          </div>
+          <div className="backgroundBox">
+            I am a first year Mechatronics Engineering student at the University of Waterloo.
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Introduction
