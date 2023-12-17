e353de1f2a05f8baa39f19584315fe3745ef4dca
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-12-17 16:48:24 -0500

    Working on experiences page

---
+++ src/app/Components/Experiences/experienceBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./experienceBox.css"
+
+const experiencesBox = () => {
+  return (
+    <div className='experiencesBox'>
+        <div className="projectBackground">
+            <div className="titleBox">
+                PortfolioBase
+            </div>
+            <div className="description">
+                PortfolioBase is a platform that connects design students with agencies for short-term and entry-level employment opportunities.
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default experiencesBox;
