29e3d22a33919ea3a7e3e125882c874f50f810a8
adityaajay33 <107756503+adityaajay33@users.noreply.github.com>
2023-12-22 20:07:06 -0500

    added socials

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -11,7 +11,18 @@ const year = getCurrentYear();
 
 const Footer = () => {
   return (
-    <div className='footerHome'>Copyright © {year} | Aditya Ajay</div>
+    <div className="footerMain">
+      <div className="socialLinks">
+      <a href="https://github.com/adityaajay33" target="_blank" rel="noopener noreferrer">Github</a>
+      <a href="https://www.linkedin.com/in/adityaajay33/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+      <a href="https://devpost.com/aditya-ajay33?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target="_blank" rel="noopener noreferrer">Devpost</a>
+      </div>
+      <div className='footerCR'>
+      
+        Copyright © {year} | Aditya Ajay
+      
+      </div>
+    </div>
   )
 }
 
